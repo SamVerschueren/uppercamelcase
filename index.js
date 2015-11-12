8dd7f8869336c15fb302c1f3fdc3285748622226
Sam Verschueren <sam.verschueren@gmail.com>
2015-11-12 19:21:12 +0100

    :arrow_up: camelcase

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
 'use strict';
-
-/**
- * Returns the UpperCamelCase of the string passed in.
- *
- * @author Sam Verschueren      <sam.verschueren@gmail.com>
- * @since  17 April 2015
- */
-
-// module dependencies
 var camelCase = require('camelcase');
 
-module.exports = function(str) {
-    var cased = camelCase(str);
-
-    return cased.charAt(0).toUpperCase() + cased.slice(1);
+module.exports = function () {
+	var cased = camelCase.apply(camelCase, arguments);
+	return cased.charAt(0).toUpperCase() + cased.slice(1);
 };
