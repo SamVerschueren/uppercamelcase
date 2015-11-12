d3f0335b4ac30c187882eb7ed3830d2092ac3c62
Sam Verschueren <sam.verschueren@gmail.com>
2015-11-12 19:21:32 +0100

    XO and AVA

--- test.js
+++ test.js
@@ -1,161 +1,38 @@
-'use strict';
+import test from 'ava';
+import fn from './';
 
-/**
- * All the tests for the UpperCamelCase library.
- *
- * @author Sam Verschueren      <sam.verschueren@gmail.com>
- * @since  17 April 2015
- */
-
-// module dependencies
-var chai = require('chai'),
-    upperCamelCase = require('./');
-
-chai.should();
-
-describe('UpperCamelCase', function() {
-
-    it('Should return \'Foo\' for \'foo\'', function() {
-        upperCamelCase('foo').should.be.equal('Foo');
-    });
-
-    it('Should return \'FooBar\' for \'fooBar\'', function() {
-        upperCamelCase('fooBar').should.be.equal('FooBar');
-    });
-
-    it('Should return \'FoobarBaz\' for \'fooBar-Baz\'', function() {
-        upperCamelCase('fooBar-Baz').should.be.equal('FoobarBaz');
-    });
-
-    describe('Dashes', function() {
-        it('Should return \'FooBar\' for \'foo-bar\'', function() {
-            upperCamelCase('foo-bar').should.be.equal('FooBar');
-        });
-
-        it('Should return \'FooBarBaz\' for \'foo-bar-baz\'', function() {
-            upperCamelCase('foo-bar-baz').should.be.equal('FooBarBaz');
-        });
-
-        it('Should return \'FooBar\' for \'foo--bar\'', function() {
-            upperCamelCase('foo--bar').should.be.equal('FooBar');
-        });
-
-        it('Should return \'FooBar\' for \'--foo-bar\'', function() {
-            upperCamelCase('--foo-bar').should.be.equal('FooBar');
-        });
-
-        it('Should return \'FooBar\' for \'--foo--bar\'', function() {
-            upperCamelCase('--foo--bar').should.be.equal('FooBar');
-        });
-
-        it('Should return \'FooBar\' for \'FOO-BAR\'', function() {
-            upperCamelCase('FOO-BAR').should.be.equal('FooBar');
-        });
-
-        it('Should return \'FooBar\' for \'-foo-bar-\'', function() {
-            upperCamelCase('-foo-bar-').should.be.equal('FooBar');
-        });
-
-        it('Should return \'FooBar\' for \'--foo--bar--\'', function() {
-            upperCamelCase('--foo--bar--').should.be.equal('FooBar');
-        });
-
-        it('Should return \'-\' for \'-\'', function() {
-            upperCamelCase('-').should.be.equal('-');
-        });
-
-        it('Should return \'-\' for \' - \'', function() {
-            upperCamelCase(' - ').should.be.equal('-');
-        });
-    });
-
-    describe('Dots', function() {
-        it('Should return \'FooBar\' for \'foo.bar\'', function() {
-            upperCamelCase('foo.bar').should.be.equal('FooBar');
-        });
-
-        it('Should return \'FooBarBaz\' for \'foo.bar.baz\'', function() {
-            upperCamelCase('foo.bar.baz').should.be.equal('FooBarBaz');
-        });
-
-        it('Should return \'FooBar\' for \'foo..bar\'', function() {
-            upperCamelCase('foo..bar').should.be.equal('FooBar');
-        });
-
-        it('Should return \'FooBar\' for \'..foo.bar\'', function() {
-            upperCamelCase('..foo.bar').should.be.equal('FooBar');
-        });
-
-        it('Should return \'FooBar\' for \'..foo..bar\'', function() {
-            upperCamelCase('..foo..bar').should.be.equal('FooBar');
-        });
-
-        it('Should return \'FooBar\' for \'.foo.bar.\'', function() {
-            upperCamelCase('.foo.bar.').should.be.equal('FooBar');
-        });
-
-        it('Should return \'FooBar\' for \'..foo..bar..\'', function() {
-            upperCamelCase('..foo..bar..').should.be.equal('FooBar');
-        });
-    });
-
-    describe('Underscores', function() {
-        it('Should return \'FooBar\' for \'foo_bar\'', function() {
-            upperCamelCase('foo_bar').should.be.equal('FooBar');
-        });
-
-        it('Should return \'FooBarBaz\' for \'foo_bar_baz\'', function() {
-            upperCamelCase('foo_bar_baz').should.be.equal('FooBarBaz');
-        });
-
-        it('Should return \'FooBar\' for \'foo__bar\'', function() {
-            upperCamelCase('foo__bar').should.be.equal('FooBar');
-        });
-
-        it('Should return \'FooBar\' for \'__foo_bar\'', function() {
-            upperCamelCase('__foo_bar').should.be.equal('FooBar');
-        });
-
-        it('Should return \'FooBar\' for \'__foo__bar\'', function() {
-            upperCamelCase('__foo__bar').should.be.equal('FooBar');
-        });
-
-        it('Should return \'FooBar\' for \'_foo_bar_\'', function() {
-            upperCamelCase('_foo_bar_').should.be.equal('FooBar');
-        });
-
-        it('Should return \'FooBar\' for \'__foo__bar__\'', function() {
-            upperCamelCase('__foo__bar__').should.be.equal('FooBar');
-        });
-    });
-
-    describe('Spaces', function() {
-        it('Should return \'FooBar\' for \'foo bar\'', function() {
-            upperCamelCase('foo bar').should.be.equal('FooBar');
-        });
-
-        it('Should return \'FooBarBaz\' for \'foo bar baz\'', function() {
-            upperCamelCase('foo bar baz').should.be.equal('FooBarBaz');
-        });
-
-        it('Should return \'FooBar\' for \'foo  bar\'', function() {
-            upperCamelCase('foo  bar').should.be.equal('FooBar');
-        });
-
-        it('Should return \'FooBar\' for \'  foo bar\'', function() {
-            upperCamelCase('  foo bar').should.be.equal('FooBar');
-        });
-
-        it('Should return \'FooBar\' for \'  foo  bar\'', function() {
-            upperCamelCase('  foo  bar').should.be.equal('FooBar');
-        });
-
-        it('Should return \'FooBar\' for \' foo bar \'', function() {
-            upperCamelCase(' foo bar ').should.be.equal('FooBar');
-        });
-
-        it('Should return \'FooBar\' for \'  foo  bar  \'', function() {
-            upperCamelCase('  foo  bar  ').should.be.equal('FooBar');
-        });
-    });
+test('camelCase', t => {
+	t.is(fn('foo'), 'Foo');
+	t.is(fn('foo-bar'), 'FooBar');
+	t.is(fn('foo-bar-baz'), 'FooBarBaz');
+	t.is(fn('foo--bar'), 'FooBar');
+	t.is(fn('--foo-bar'), 'FooBar');
+	t.is(fn('--foo--bar'), 'FooBar');
+	t.is(fn('FOO-BAR'), 'FooBar');
+	t.is(fn('-foo-bar-'), 'FooBar');
+	t.is(fn('--foo--bar--'), 'FooBar');
+	t.is(fn('foo.bar'), 'FooBar');
+	t.is(fn('foo..bar'), 'FooBar');
+	t.is(fn('..foo..bar..'), 'FooBar');
+	t.is(fn('foo_bar'), 'FooBar');
+	t.is(fn('__foo__bar__'), 'FooBar');
+	t.is(fn('__foo__bar__'), 'FooBar');
+	t.is(fn('foo bar'), 'FooBar');
+	t.is(fn('  foo  bar  '), 'FooBar');
+	t.is(fn('-'), '-');
+	t.is(fn(' - '), '-');
+	t.is(fn('fooBar'), 'FooBar');
+	t.is(fn('fooBar-baz'), 'FoobarBaz');
+	t.is(fn('F'), 'F');
+	t.is(fn('Foo'), 'Foo');
+	t.is(fn('FOO'), 'Foo');
+	t.is(fn('foo', 'bar'), 'FooBar');
+	t.is(fn('foo', '-bar'), 'FooBar');
+	t.is(fn('foo', '-bar', 'baz'), 'FooBarBaz');
+	t.is(fn('', ''), '');
+	t.is(fn('--'), '');
+	t.is(fn(''), '');
+	t.is(fn('--__--_--_'), '');
+	t.is(fn('---_', '--', '', '-_- '), '');
+	t.end();
 });
